86ae3b3a68485c5df6529727773f0659ed946f59
Christian Gawron <gawron.christian@fh-swf.de>
2021-04-26 11:02:04 +0200

    fix: read token from environment

--- CO2DataWriter/Writer.py
+++ CO2DataWriter/Writer.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import threading
 from random import randint
+import os
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate a Token from the "Tokens Tab" in the UI
-token = ""
+token = os.environ['TOKEN']
 org = "fhswf"
 bucket = "timeseries"
 
