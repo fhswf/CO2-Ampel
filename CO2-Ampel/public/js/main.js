98c563c5e218d1a0391ed0fef44007a285c64d6a
Christian Gawron <gawron.christian@fh-swf.de>
2021-04-26 07:55:51 +0200

    fix: make URLs relative for deployment (basepath)

--- CO2-Ampel/public/js/main.js
+++ CO2-Ampel/public/js/main.js
@@ -1,16 +1,16 @@
 let interval = null;
 
-function getAvg(data){
+function getAvg(data) {
     let sum = 0;
 
-    for(let element in data){
+    for (let element in data) {
         sum += data[element].co2;
     }
 
     return sum / data.length;
 }
 
-function changeTrafficLight(data){
+function changeTrafficLight(data) {
     let avg = getAvg(data);
     let green = document.getElementById("green_light");
     let yellow = document.getElementById("yellow_light");
@@ -20,10 +20,10 @@ function changeTrafficLight(data){
     yellow.classList.remove("enabled");
     red.classList.remove("enabled");
 
-    if(avg < 1000){
+    if (avg < 1000) {
         green.classList.add("enabled");
     }
-    else if(avg < 2000){
+    else if (avg < 2000) {
         yellow.classList.add("enabled");
     }
     else {
@@ -31,11 +31,11 @@ function changeTrafficLight(data){
     }
 }
 
-function renderData(data){
+function renderData(data) {
     let dataTable = document.getElementById("dataTable");
     dataTable.innerHTML = "";
 
-    if(data.length === 0){
+    if (data.length === 0) {
         document.getElementById("data_not_found").classList.remove("hidden");
         document.getElementById("data_table").classList.add("hidden");
         document.getElementById("device_not_found").classList.add("hidden");
@@ -44,7 +44,7 @@ function renderData(data){
         document.getElementById("device_not_found").classList.add("hidden");
         document.getElementById("data_table").classList.remove("hidden");
 
-        for(let element in data){
+        for (let element in data) {
             let newEL = document.createElement("tr");
             let timestamp = document.createElement("td");
             let co2 = document.createElement("td");
@@ -65,41 +65,41 @@ function renderData(data){
     }
 }
 
-function clearFetch(){
-    if(interval !== null){
+function clearFetch() {
+    if (interval !== null) {
         clearInterval(interval);
     }
 }
 
-function clearTable(){
+function clearTable() {
     clearFetch();
     document.getElementById("device_not_found").classList.add("hidden");
     document.getElementById("data_not_found").classList.add("hidden");
     document.getElementById("data_table").classList.add("hidden");
 }
 
-function fetchData(){
+function fetchData() {
     let params = {
         limit: document.getElementById('limit').value
     };
 
-    if(document.getElementById("start_date").value !== "" && document.getElementById("start_time").value !== ""){
+    if (document.getElementById("start_date").value !== "" && document.getElementById("start_time").value !== "") {
         params["start"] = document.getElementById("start_date").value + "T" + document.getElementById("start_time").value + "Z";
-    } else if(document.getElementById("start_date").value !== ""){
+    } else if (document.getElementById("start_date").value !== "") {
         params["start"] = document.getElementById("start_date").value + "T00:00:00Z";
     }
 
-    if(document.getElementById("end_date").value !== "" && document.getElementById("end_time").value !== ""){
+    if (document.getElementById("end_date").value !== "" && document.getElementById("end_time").value !== "") {
         params["end"] = document.getElementById("end_date").value + "T" + document.getElementById("end_time").value + "Z";
-    } else if(document.getElementById("end_date").value !== ""){
+    } else if (document.getElementById("end_date").value !== "") {
         params["end"] = document.getElementById("end_date").value + "T23:59:59Z";
     }
 
-    if(document.getElementById("host_name").value !== ""){
+    if (document.getElementById("host_name").value !== "") {
         params["host"] = document.getElementById("host_name").value;
     }
 
-    fetch("/api/data?" + new URLSearchParams(params)).then((data) => {
+    fetch("./api/data?" + new URLSearchParams(params)).then((data) => {
         return data.json();
     }).then((data) => {
         renderData(data);
